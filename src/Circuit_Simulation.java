79f1d52e7b70362570db97922d8ff1652dc77df7
amiralirz <65935330+amiralirz@users.noreply.github.com>
2020-06-12 17:52:47 +0430

    Commit Message

--- src/Circuit_Simulation.java
+++ src/Circuit_Simulation.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
+
 public class Circuit_Simulation {
+    class element{
+        int n1,n2;
+    }
+    class node{
+        ArrayList<element> connections = new ArrayList<element>();
+        float voltage;
+        boolean is_ground = false;
+    }
+    class circuit{
+        ArrayList<node> nodes = new ArrayList<node>();
+        ArrayList<element> elements = new ArrayList<element>();
+    }
     public static void main(String[] args){
         System.out.println("Hello World");
     }
