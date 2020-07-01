67fcb8f59411421ded6f2a584050d83824c68167
amiralirz <65935330+amiralirz@users.noreply.github.com>
2020-07-01 19:38:41 +0430

    input and classes were added

--- src/Circuit_Simulation.java
+++ src/Circuit_Simulation.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -10,50 +12,89 @@ public class Circuit_Simulation {
         boolean is_ground = false;
     }
     class element{
-        int n1,n2;
+        String name;
+        int n1,n2,b,voltage;
     }
     class branch{
         ArrayList<node> connections = new ArrayList<node>();
         String name;
 
     }
-    class circuit{
-        ArrayList<node> nodes = new ArrayList<node>();
-        ArrayList<element> elements = new ArrayList<element>();
-    }
     /*********************elements*********************/
     {
         class resistor extends element{
+            int r;
+            void vu(int Current){
 
+            }
+            void cu(int Voltage){
+
+            }
         }
+
         class capacitor extends element{
+            int c;
+            int q1,q2;
+            void vu(int Current,int dt){
+
+            }
+            void cu(int Voltage){
+
+            }
+        }
+
+        class inductor extends element{
+            int l;
+            int f1,f2;
+            void vu(int Current, int dt){
+
+            }
+        }
+
+        class voltage_Source extends element{
 
         }
-        class inductor extends element{
 
+        class current_Source extends element{
+
+        }
+
+        class diode extends element{
+            int direction;
+            boolean alone_Element_in_Branch;
+            String model;
+            int[] parameters;
         }
     }
 
-    public static void main(String[] args){
-        boolean test = false;
-        File f;
-        if(!test){
-            f = new File("sample_input.txt");
+    static class circuit{
+        boolean test = false,has_file = false;
+        ArrayList<node> nodes = new ArrayList<node>();
+        ArrayList<element> elements = new ArrayList<element>();
+        File file;
+        void get_file_input(){
+            if(!test){
+                file = new File("D:\\Projects\\Java\\oop\\Spice");
+                JFrame f1 = new JFrame("Input file selection");
+                FileSystemView fsv = FileSystemView.getFileSystemView();
+                JFileChooser jfc = new JFileChooser(file,fsv);
+                int response = jfc.showOpenDialog(f1);
+                if(response == JFileChooser.APPROVE_OPTION){
+                    file = jfc.getSelectedFile();
+                    has_file = true;
+                }
+            }
+            else {
+                file = new File("sample_input.txt");
+                has_file = true;
+            }
         }
-        else {
-            f = new File("sample_input.txt");
-        }
-        Scanner con;
-        try {
-            con = new Scanner(f);
-        }
-        catch (FileNotFoundException e){
-            System.out.println(e);
-        }
-        String input1 = "";
-        //v : independent voltage source -- i : independent current source r : resistor
-        switch (input1.charAt(0)){
 
-        }
+    }
+
+    public static void main(String[] args){
+        circuit c1 = new circuit();
+        while (!c1.has_file)
+        c1.get_file_input();
     }
 }
